670098adc2e2fd626a8861d60419909228dfba37
Samyak Khairnar <samyakkhairnar@rocketmail.com>
2022-11-29 21:05:40 +0530

    All Done with Javascript

--- app.js
+++ app.js
@@ -5,7 +5,36 @@ const luckReturn = document.querySelector("#check-luck");
 const message = document.querySelector("#message");
 
 
-checkButton.addEventListener("click",function checkLuck(){
+checkButton.addEventListener("click",function validateInput(){
     
+    if(luckyNumber.value=="" || birthDate.value==""){
+        luckReturn.style.display = "none";
+        message.innerHTML="Both Inputs are Required to Know Your Luck";
+    }else{
+        luckReturn.style.display = "block";
+        const newDate = birthDate.value.replaceAll("-","") 
+        const sum = calculateSum(newDate);
+        checkLuck(sum,parseInt(luckyNumber.value));
+    }
 
 });
+
+function calculateSum(dob){
+    let sum = 0;
+    for(let i=0;i<dob.length;i++){
+        sum = sum + (dob.charAt(i));
+    }
+    return sum;
+}
+
+function checkLuck(dob,lucky){
+    var abc = dob%lucky;
+    console.log(abc);
+    if(dob%lucky===0){
+        luckReturn.innerHTML="You're Very Lucky !";
+        message.innerHTML="";
+    }else{
+        luckReturn.innerHTML="Seems that You're Not Lucky";
+        message.innerHTML="This was Just for Fun, No Numerology Added !";
+    }
+}
