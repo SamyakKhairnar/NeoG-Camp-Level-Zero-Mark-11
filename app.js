f2a0a9b576c56ddec005d33b93a944509023c451
Samyak Khairnar <samyakkhairnar@rocketmail.com>
2022-11-29 20:00:47 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const birthDate = document.querySelector("#birth-date");
+const luckyNumber = document.querySelector("#lucky-no");
+const checkButton = document.querySelector("#check-btn")
+const luckReturn = document.querySelector("#check-luck");
+const message = document.querySelector("#message");
+
+
+checkButton.addEventListener("click",function checkLuck(){
+    
+
+});
